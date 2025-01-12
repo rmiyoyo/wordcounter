93d8e65e4a46e72c24ace7f2bd82862829e0a0d8
rmiyoyo <miyoyoraphael@gmail.com>
2025-01-12 19:48:47 +0300

    Add Word Count Char Count options

--- main.go
+++ main.go
@@ -6,25 +6,39 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	words := flag.Bool("w", false, "Count words")
+	chars := flag.Bool("c", false, "Count characters")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+
+	if !*lines && !*words && !*chars {
+		*lines = true
+	}
+
+	fmt.Println(count(os.Stdin, *lines, *words, *chars))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines, countWords, countChars bool) int {
 	scanner := bufio.NewScanner(r)
 
-	if !countLines {
+	if !countWords {
 		scanner.Split(bufio.ScanWords)
+	} else if !countLines {
+		scanner.Split(bufio.ScanRunes)
 	}
 
 	wc := 0
 
 	for scanner.Scan() {
-		wc++
+		if countChars {
+			wc += len(strings.TrimSpace(scanner.Text()))
+		} else {
+			wc++
+		}
 	}
 
 	return wc
