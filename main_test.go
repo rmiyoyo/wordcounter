d585f33195f049e9d21d09c148f1cb9297bad8d4
rmiyoyo <miyoyoraphael@gmail.com>
2025-01-12 19:58:36 +0300

    Update tests to include lines and characters

--- main_test.go
+++ main_test.go
@@ -5,10 +5,30 @@ import (
 	"testing"
 )
 
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("Line1\nLine2\nLine3\n")
+	exp := 3
+	res := count(b, true, false, false)
+
+	if res != exp {
+		t.Errorf("Expected %d lines, got %d.\n", exp, res)
+	}
+}
+
 func TestCountWords (t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b, false)
+	res := count(b, false, true, false)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d.\n", exp, res)
+	}
+}
+
+func TestCountChars (t *testing.T) {
+	b := bytes.NewBufferString("Test Chars\n")
+	exp := 10
+	res := count(b, false, false, true)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d.\n", exp, res)
